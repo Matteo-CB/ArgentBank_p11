820d8f4fdcb2ff9325628f523ace20d1b512ef8e
Chanté-Biyikli <matteo.chbiyikli@gmail.com>
2023-10-10 10:42:15 +0200

    correction acces /profil

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -8,39 +8,50 @@ import axios from "axios";
 import { changeData } from "./features/CurrentProfile/currentProfileSlice";
 
 const App = () => {
-  const body= document.querySelector('body')
-    if(body !== null){
+  const body = document.querySelector("body");
+  if (body !== null) {
+    body.style.overflow = "auto";
+  }
+  const isConnected = useSelector((state: any) => state.isConnected.value);
+  const dispatch = useDispatch();
+  if (
+    typeof localStorage.getItem("token") === "string" &&
+    isConnected === false
+  ) {
+    dispatch(toggleIsConnected());
+    axios
+      .post(
+        "http://localhost:3001/api/v1/user/profile",
+        localStorage.getItem("token"),
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      )
+      .then((e) => {
+        const data = {
+          firstName: e.data.body.firstName,
+          lastName: e.data.body.lastName,
+          email: e.data.body.email,
+          userName: e.data.body.userName,
+        };
 
-        body.style.overflow='auto'
-    }
-  const isConnected = useSelector((state: any) => state.isConnected.value)
-  const dispatch = useDispatch()
- if(typeof localStorage.getItem('token') === 'string' && isConnected === false){
-  dispatch(toggleIsConnected())
-  axios.post('http://localhost:3001/api/v1/user/profile', localStorage.getItem('token'), {
-    headers: {
-      Authorization:`Bearer ${localStorage.getItem('token')}`
-    }
-  }).then((e) => {
-    
-    const data = {
-      firstName: e.data.body.firstName,
-      lastName: e.data.body.lastName,
-      email: e.data.body.email,
-      userName:e.data.body.userName
-    }
-    
-    dispatch(changeData(data))
-}
-  )
- }
- 
+        dispatch(changeData(data));
+      });
+  }
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<SignIn />} />
-        <Route path="/profile" element={<User />} />
+        {isConnected ? (
+          <Route path="/profile" element={<User />} />
+        ) : (
+          <Route path="/profile" element={<SignIn />} />
+        )}
+        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
